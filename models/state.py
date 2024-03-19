4435593faef61915954c0899e588d47c79088f3f
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-19 04:06:31 -0700

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -5,10 +5,20 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from sqlalchemy.orm import relationship
-
+import models
+from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
+     if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """a list of all related City objects"""
+            cities = list()
+            for city in models.storage.all(City).items():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
