f7ec55384d0bb929c30d201b69110d1d8841741f
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-21 22:23:15 +0000

    webflask framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -38,7 +38,7 @@ def c(text):
     return "C {}".format(text)
 
 
-@app.route("python/<text>/" strict_slashes=False)
+@app.route("/python/<text>/" strict_slashes=False)
 def python(text="is cool"):
     """display 'Python', followed by the value of the text variable
        (replace underscore _ symbols with a space )
