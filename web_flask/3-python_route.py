d635ab695806d4c5fc34cc4abc22ceed71fb9175
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-21 22:31:15 +0000

    webflask framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -38,10 +38,11 @@ def c(text):
     return "C {}".format(text)
 
 
+@app.route("/python/")
 @app.route("/python/(<text>)", strict_slashes=False)
 def python(text="is cool"):
-    """display 'Python', followed by the value of the text variable
-       (replace underscore _ symbols with a space )
+    """display 'Python', followed by the value of the text
+       variable (replace underscore _ symbols with a space )
        The default value of text is 'is cool'
     """
     text = text.replace("_", " ")
