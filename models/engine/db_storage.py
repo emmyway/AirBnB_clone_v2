e2673200701a6136f829365b5ac371b74cee89af
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-21 23:41:10 +0000

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -63,3 +63,7 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """method on the private session attribute"""
+        self.__session.close()
